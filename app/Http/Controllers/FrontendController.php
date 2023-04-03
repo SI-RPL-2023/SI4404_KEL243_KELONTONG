d481bf5beca93d84db3efc3160d544952a3c4b0f
Abidzarzlfr <abidzarbijeh@gmail.com>
2023-04-03 23:07:13 +0700

    commit onboarding, update blade template ref #1

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class FrontendController extends Controller
 {
     public function index(){
-        return view('frontend.layouts.frontend');
+        return view('front.home');
     }
 }
---
+++ resources/views/front/includes/footer.blade.php
@@ -0,0 +1,15 @@
+<div class="footer p-4 mt-5" style="background-color: #CD8C3F">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    Our Contact :
+                </div>
+                <div class="col-md-4">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae porro ratione magnam optio temporibus quaerat, culpa harum, earum minima, quo alias at.
+                </div>
+                <div class="col-md-4">
+                    @ Copyright KELONTONG 2023
+                </div>
+            </div>
+        </div>
+    </div>
