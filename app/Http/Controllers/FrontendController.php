73d7d0ce9471fef5dfaddd1b2a0360db3e0996de
farishisyamm <xxia321@gmail.com>
2023-04-16 07:48:40 +0700

    Limit only 6 warung shown. ref #1

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class FrontendController extends Controller
 {
     public function index(){
-        $warungs = Warung::all();
+        $warungs = Warung::where('verif_status', '=', 'verified')->take(6)->get();
         return view ('front.home', ['warungs' => $warungs]);
     }
 }
