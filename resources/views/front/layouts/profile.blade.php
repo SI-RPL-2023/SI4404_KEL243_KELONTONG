33f7d8e7c00f7c22be9dc5021dedb43eaa17840d
Abidzarzlfr <abidzarbijeh@gmail.com>
2023-05-08 13:21:24 +0700

    update profile ref #5

--- resources/views/front/layouts/profile.blade.php
+++ resources/views/front/layouts/profile.blade.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Bootstrap 5.2 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+  <!-- Link to CSS -->
+  <link rel="stylesheet" href="{{ asset('frontend/style.css') }}">
+
+  <!-- Poppins Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;800;900&display=swap" rel="stylesheet">
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Profile Page</title>
@@ -21,7 +32,7 @@
 
     form {
       margin-top: 20px;
-      text-align: center;
+      text-align: left;
     }
 
     button {
@@ -31,6 +42,7 @@
     .submit-button {
       background-color: #CD8C3F;
       color: #FFFFFF;
+      margin-top: 20px;
     }
   </style>
 </head>
@@ -38,26 +50,101 @@
   <div class="container-fluid container-custom">
     <div class="profile-container">
       <h2>Profile</h2>
-      <img src="profile.png" alt="Profile" class="rounded-circle" style="width: 150px; height: 150px;">
+      <img src="profile.png" alt="img" class="rounded-circle mt-3" style="width: 150px; height: 150px;">
       <form>
         <div class="mb-3">
-          <input type="email" class="form-control" id="email" placeholder="Email">
+          <label for="email" class="form-label">Email</label>
+          <input type="email" class="form-control" id="email" name="email" required>
         </div>
         <div class="mb-3">
-          <input type="text" class="form-control" id="name" placeholder="Name">
+          <label for="name" class="form-label">Name</label>
+          <input type="name" class="form-control" id="name" name="name" required>
         </div>
         <div class="mb-3">
-          <input type="tel" class="form-control" id="phone" placeholder="Phone">
+          <label for="phone" class="form-label">Phone</label>
+          <input type="phone" class="form-control" id="phone" phone="phone" required>
         </div>
         <div class="mb-3">
-          <input type="password" class="form-control" id="password" placeholder="Password">
+          <label for="password" class="form-label">Password</label>
+          <input type="password" class="form-control" id="password" password="password" required>
         </div>
-        <button type="submit" class="btn btn-primary submit-button">Submit</button>
       </form>
+      <button type="submit" class="btn btn-primary submit-button">Submit</button>
       <button type="button" class="btn btn-secondary mt-3">Logout</button>
     </div>
+      
   </div>
-  
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.min.js"></script>
+  <!-- Bootstrap Script -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </body>
 </html>
+
+
+<!-- <!doctype html>
+<html lang="en"> -->
+
+<!-- <head> -->
+    <!-- <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Kelontong</title> -->
+
+    <!-- Bootstrap 5.2 -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
+
+    <!-- Link to CSS -->
+    <!-- <link rel="stylesheet" href="{{ asset('frontend/style.css') }}"> -->
+
+    <!-- Poppins Fonts -->
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;800;900&display=swap" rel="stylesheet"> -->
+
+<!-- </head> -->
+
+<!-- <body> -->
+    <!-- Login Page -->
+    <!-- <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-md-6 min-vh-100 banner">
+                <img src="{{('frontend/logres/logres.png')}}" alt="login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+            </div>
+            <div class="col-md-6">
+                <div class="m-auto" style="width: 90%">
+                    <h2 class="fw-bold mb-4">Login</h2> -->
+                    <!-- Form -->
+                    <!-- <form action="{{ route('login')}}" method="post"> -->
+                        <!-- @csrf -->
+                        <!-- Email -->
+                        <!-- <div class="mb-3 position-relative">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                        </div> -->
+                        <!-- Password -->
+                        <!-- <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div> -->
+                        <!-- Remember me -->
+                        <!-- <div class="form-check mb-3">
+                            <input class="form-check-input" type="checkbox" id="remember" name="remember">
+                            <label class="form-check-label" for="remember">Remember me</label>
+                        </div> -->
+                        <!-- Login Button -->
+                        <!-- <button type="submit" class="btn btn-dark px-4 mt-2" style="background-color: #CD8C3F;">Login</button>
+
+                    </form> -->
+                    <!-- Link ke register page -->
+                    <!-- <p class="mt-3">Anda belum memiliki akun? <a href="/register">Register</a></p>
+                </div>
+            </div>
+        </div> -->
+    <!-- </div> -->
+
+    <!-- Bootstrap Script -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> -->
+<!-- </body> -->
+
+<!-- </html> -->
