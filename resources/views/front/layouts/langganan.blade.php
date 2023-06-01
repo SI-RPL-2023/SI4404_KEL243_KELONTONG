55b152bfd20a0e4a3d07e28bc04d8a2d3c2000b2
farishisyamm <xxia321@gmail.com>
2023-06-02 01:31:22 +0700

    function index SubscribeController ref #32

--- resources/views/front/layouts/langganan.blade.php
+++ resources/views/front/layouts/langganan.blade.php
@@ -36,42 +36,7 @@
 
 <body>
     <!-- Navbar -->
-    <nav class="navbar sticky-top navbar-expand-lg" style="background-color: #CD8C3F">
-        <div class="container py-2 fw-semibold">
-
-            <a class="navbar-brand" href="#">
-                <img src="frontend/logo/kelontong.png" width="147" height="51" alt="" loading="lazy">
-            </a>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-1">
-                    <li class="nav-item ms-4">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item ms-4">
-                        <a class="nav-link active" aria-current="page" href="#">About Us</a>
-                    </li>
-                    <li class="nav-item ms-4">
-                        <a class="nav-link active" aria-current="page" href="#">List Kelontong</a>
-                    </li>
-                    <li class="nav-item ms-4">
-                        <a class="nav-link active" aria-current="page" href="#">Update to Seekers</a>
-                    </li>
-                    <li class="nav-item ms-4">
-                        <a class="nav-link active" aria-current="page" href="#">Langganan</a>
-                    </li>
-                </ul>
-                <form action={{route('login')}} method="GET" class="d-flex">
-                    @csrf
-                    <!-- <button class="btn btn-light fw-semibold" type="submit">Login</button> -->
-                    <img src="frontend/profile/example.png" alt="Profile" class="rounded-circle" style="width: 40px;">
-                </form>
-            </div>
-        </div>
-    </nav>
+    @include('front.includes.navbar')
 
     <!-- Langganan -->
     <div class="header" style="background-color: #442100;">
@@ -109,7 +74,9 @@
             </div>
             <!-- ! Button -->
             <div class="center-button">
-                <button type="submit" class="btn btn-dark mb-3" style="background-color: #CD8C3F;">Pilih Paket</button>
+                <<form action="{{ route('payment') }}" method="GET">
+                    <button type="submit" class="btn btn-dark mb-3" style="background-color: #CD8C3F;">Pilih Paket</button>
+                </form>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\ApplySeekerController;
 use App\Http\Controllers\ListWarungController;
 use App\Http\Controllers\InputWarungController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\SubscribeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -48,4 +49,5 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/admin', [AdminController::class, 'edit'])->name('viewAdmin');
     Route::put('/admin/warung/{id}', [AdminController::class, 'updateWarung'])->name('updateWarung');
     Route::put('/admin/user/{id}', [AdminController::class, 'updateUser'])->name('updateUser');
+    Route::get('/subscribe', [SubscribeController::class, 'index'])->name('subscribe');
 });
