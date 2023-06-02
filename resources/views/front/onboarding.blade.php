f3a9df23142701b0906012f80da0cf8c42d82165
farishisyamm <xxia321@gmail.com>
2023-06-02 14:33:26 +0700

    edit about us

--- resources/views/front/onboarding.blade.php
+++ resources/views/front/onboarding.blade.php
@@ -5,7 +5,10 @@
 <div id="about">
 <p class="h1 fw-semibold text-center mt-5">About Us</p>
 <div class="subheading">
-    <p class="text-center mt-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit vel odio ligula dolor bibendum diam lacus morbi volutpat. Amet consequat tellus proin volutpat risus nulla. Nullam molestie eu sem fames lectus metus proin ultricies. Commodo diam pretium in sed neque, venenatis. Nunc nisl metus tincidunt adipiscing et quis. Mattis orci feugiat condimentum vel, at ullamcorper nibh placerat. Sed cursus urna facilisis varius tristique. Elit lobortis cursus faucibus suspendisse nulla aenean maecenas et. Consequat dui eros, eget eu odio euismod. Non id rhoncus tempor ut pharetra enim dictumst tristique. Bibendum suspendisse pulvinar lacus felis volutpat egestas commodo a quam. Malesuada tempus rhoncus amet fermentum nunc Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa tempora asperiores eaque dolorum quo blanditiis porro, voluptatibus, adipisci veritatis natus consequatur? Eveniet corporis nesciunt natus nostrum placeat beatae vel nihil. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste sequi cum ea repellendus hic, enim esse eveniet ut provident officiis, voluptatem quisquam expedita earum facilis iure minus sed officia aperiam.</p>
+    <p class="text-center mt-5 mb-5">Kelontong merupakan website yang mengumpulkan dan menyediakan data tentang warung kelontong secara lengkap dan mudah untuk diakses.
+        Dengan adanya kelontong pada distribusi barang ke warung kelontong dapat meningkatkan efisiensi dan efektivitasnya, dengan adanya 
+        hal tersebut pastinya akan berdampak postif bagi perekonomian bersifat mikro dan makro yang ada di Indonesia, serta hal tersebut
+        dapat meningkatkan daya saing bagi usah kecil dan menengah di sektor perdagangan.</p>
 </div>
 </div>
 <!-- Card -->
